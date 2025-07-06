a284631011481819db23405fbbb12dcbbd15f3ab
Bakhshish987 <157077850+Bakhshish987@users.noreply.github.com>
2025-07-06 19:57:04 +0530

    Update Skills.js

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -1,18 +1,6 @@
 import React from 'react';
 import { FaPython, FaGitAlt, FaFileExcel, FaChartBar } from "react-icons/fa";
-import {
-  SiPandas,
-  SiNumpy,
-  SiScikitlearn,
-  SiStreamlit,
-  SiAmazonaws,
-  SiMysql,
-  SiVercel,
-  SiJupyter,
-  SiTensorflow,
-  SiPytorch,
-  SiPlotly
-} from "react-icons/si";
+import { SiPandas, SiNumpy, SiScikitlearn, SiStreamlit, SiAmazonaws, SiMysql, SiVercel } from "react-icons/si";
 
 const Skills = ({ skill }) => {
   const icon = {
@@ -26,15 +14,11 @@ const Skills = ({ skill }) => {
     'Streamlit': <SiStreamlit />,
     'Git': <FaGitAlt />,
     'Vercel': <SiVercel />,
-    'AWS': <SiAmazonaws />,
-    'Jupyter Notebook': <SiJupyter />,
-    'TensorFlow': <SiTensorflow />,
-    'PyTorch': <SiPytorch />,
-    'Matplotlib': <SiPlotly />  // Close visual match
+    'AWS': <SiAmazonaws />
   };
 
   return (
-    <div title={skill} style={{ textAlign: 'center', margin: '10px' }}>
+    <div title={skill} className='SkillBox' style={{ textAlign: 'center', margin: '10px' }}>
       <div style={{ fontSize: '2rem' }}>{icon[skill] || <span>{skill}</span>}</div>
       <div style={{ fontSize: '0.8rem', marginTop: '5px' }}>{skill}</div>
     </div>
