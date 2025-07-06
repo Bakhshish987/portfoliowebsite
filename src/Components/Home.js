03807dff71e148a99be8270b0a40d502f069048e
Bakhshish987 <157077850+Bakhshish987@users.noreply.github.com>
2025-07-06 16:53:47 +0530

    Update Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import Lottie from  "lottie-react";
+import Lottie from "lottie-react";
 import SpaceBoy from "../LottieFiles/SpaceBoy.json";
 import Typed from "./Typed.js";
 import Tilt from 'react-parallax-tilt';
 import Avatar from '../images/Avatar.png';
-import {CiCoffeeCup} from "react-icons/ci";
+import { CiCoffeeCup } from "react-icons/ci";
 
 const Home = () => {
   return (
-    <div >
+    <div>
       <div className='HomePage'>
 
         <div className='HomeText'>
           <h1>Hi There!</h1>
-          <h1>I'M <b>DEVANSH SAHNI</b></h1>
-          <Typed/>   
+          <h1>I'M <b>BAKHSHISH SETHI</b></h1>
+          <Typed />
         </div>
 
         <Lottie 
@@ -29,14 +29,11 @@ const Home = () => {
         <div className='AboutText'>
           <h1 className='AboutTextHeading'>Brief <b>introduction</b></h1>
           <p>
-            I love the process of changing a raw idea into a website or a product 
-            that impacts lives. 
-            I want to do work that challenges me as a developer & work that I can 
-            be proud of.<br /><br />
-            I am fluent in <b>C++</b> and know a bit of <b>Python</b> and am working on a few 
-            projects in the <b>MERN</b> stack.<br />
-            I plan to learn <b>Next.js</b>, <b>Three.js</b> and<b> Typescript</b> in the near future. <br /><br />
-            Also, I love <b>coffee</b> <CiCoffeeCup style={{scale:"1.5", rotate:"15deg"}}/>   
+            I love transforming raw data into powerful insights and building ML-based tools that actually make a difference.
+            I'm currently pursuing a <b>Bachelor of Science in Computer Science</b> at the University of Alberta, specializing in <b>Data Science and Machine Learning</b>.<br /><br />
+            I’m skilled in <b>Python, SQL, Pandas</b>, and <b>Machine Learning</b>, and I've completed internships in <b>Data Science and Business Analytics</b>.<br />
+            I'm also building out a killer project portfolio using <b>Streamlit</b>, <b>Scikit-learn</b>, <b>Tableau</b>, and more.<br /><br />
+            Also, I run on <b>coffee</b> <CiCoffeeCup style={{ scale: "1.5", rotate: "15deg" }} />
           </p>
         </div>
         <Tilt>
@@ -47,4 +44,4 @@ const Home = () => {
   )
 }
 
-export default Home
+export default Home;
