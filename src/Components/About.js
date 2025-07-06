64044ea3452b02638444acace87ac4aa247cf2ba
Bakhshish987 <157077850+Bakhshish987@users.noreply.github.com>
2025-07-06 19:22:29 +0530

    Update About.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Skills from './Skills.js';
 import Tilt from 'react-parallax-tilt';
-import Lottie from "lottie-react"
+import Lottie from "lottie-react";
 import Coder from '../LottieFiles/coder.json';
 
 const About = () => {
@@ -11,24 +11,24 @@ const About = () => {
         <div className='AboutText'>
           <h1 className='AboutTextHeading'>Get to <b>know</b> me!</h1>
           <p>
-            I'm currently a final year <b>BSc Computer Science Specialization</b> student at the University of Alberta, graduating in <b>May 2026</b>. <br/><br/>
+            I'm currently a final year <b>BSc Computer Science Specialization</b> student at the University of Alberta, graduating in <b>May 2026</b>. <br /><br />
             I’ve worked as a <b>Data Science Intern</b> at United Worldwide Couriers (New York) and have also contributed to data analytics in the financial sector.
-            My interests lie in building ML-powered applications that solve real-world problems; check out some of my projects in the Projects section. <br/><br/>
+            My interests lie in building ML-powered applications that solve real-world problems; check out some of my projects in the Projects section. <br /><br />
             I’m <b>actively seeking</b> full-time opportunities in Data Analytics, Data Science, or AI starting Summer 2026. Let’s connect, links are in the footer!
           </p>
         </div>
 
         <div>
           <Tilt>
-            <Lottie 
-              className="illustration" 
-              animationData={Coder} 
-              loop={true} 
+            <Lottie
+              className="illustration"
+              animationData={Coder}
+              loop={true}
             />
           </Tilt>
         </div>
       </div>
-      
+
       <h1 className='SkillsHeading'>Professional Skillset</h1>
       <div className='skills'>
         <Skills skill='Python' />
@@ -48,3 +48,4 @@ const About = () => {
 }
 
 export default About;
+
